29d6fa7d05fd4e16c7b86d3173075f4df6540e40
crmw <crmw@st-andrews.ac.uk>
2018-03-01 18:20:57 +0000

    added starting point class

---
+++ src/CalcDistance.java
@@ -0,0 +1,18 @@
+public class CalcDistance {
+    private StartingPoint start;
+    private FinishPoint finish;
+
+    public CalcDistance(StartingPoint start, FinishingPoint finish) {
+        this.finish = finish;
+        this.start = start;
+    }
+
+
+    public int Distance() {
+        int distance = Math.abs(finish.getRow() - start.getRow()) + Math.abs(finish.getColumn() - start.getColumn());
+        return distance;
+    }
+
+
+
+}
