7d73a940cd493184f2db27783a1499838016bad4
Josh Cho <yoopoong96@naver.com>
2018-03-01 19:01:39 +0000

    FirstLine.java => Object for first line read Line => Object for all the other lines read
    ReadFile => Reads file and stores to ArrayList
    
    a_example.in => Example File Given
    
    Main.java => updated stuff to main

---
+++ src/ReadFile.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class ReadFile {
+
+    ArrayList<Line> lines = new ArrayList<>();
+    FirstLine firstLine;
+
+    public ReadFile(String s) throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader("a_example.in"));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine(); // read first line, 6 arguments
+            String[] lineArray = line.split(" ");
+            firstLine = new FirstLine(lineArray);
+
+            // all the other lines
+            while (br.ready()) {
+                line = br.readLine(); // 5 arguments
+                lineArray = line.split(" ");
+
+                StartingPoint sp = new StartingPoint(Integer.parseInt(lineArray[0]), Integer.parseInt(lineArray[1]));
+                FinishPoint fp = new FinishPoint(Integer.parseInt(lineArray[2]), Integer.parseInt(lineArray[3]));
+                EarliestStart es = new EarliestStart(Integer.parseInt(lineArray[4]));
+                LatestFinish lf = new LatestFinish(Integer.parseInt(lineArray[5]));
+
+
+                Line objLine = new Line(sp,fp,es,lf);
+                lines.add(objLine);
+            }
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+}
+
